785bc38d3b6052e1bc96fd0028a7802b743bdb59
Sini80 <98733025+Sini80@users.noreply.github.com>
2022-02-14 12:49:26 +0000

    Add files via upload

---
+++ Tehtävä_4.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Otsikon luonti
+Console.WriteLine(" VALUUUTTALASKURI ");
+Console.WriteLine("------------------");
+// Muuttujan esittely
+int luku1;
+// kysytään lukua
+Console.Write("Anna summa euroina ? ");
+luku1 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+//Euron kysely dollareina ja puntina
+double dollari = luku1 * 1.21;
+double punta = luku1 * 0.87;
+
+// Laskee luvut ja muuttaa dollareiksí ja ounniksi eurot
+string lasku = string.Format("{0,10:0.00}",dollari);
+string lasku1 = string.Format("{0,10:0.00}", punta);
+// Tulostetaan
+Console.WriteLine(lasku + " $ ");
+Console.WriteLine(lasku1 + " £ ");
+
+
+
+
+
